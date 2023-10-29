f66870f820d2e0b51bb6ceeeb042a88206a82510
harry.cpp <harry.cpp@outlook.com>
2023-10-29 22:49:26 +0100

    Try to blindly setup linux instructions...

--- build/BuildLinuxTask.cs
+++ build/BuildLinuxTask.cs
@@ -9,6 +9,19 @@ public sealed class BuildLinuxTask : FrostingTask<BuildContext>
 
     public override void Run(BuildContext context)
     {
-        
+        var cflags = "-Wno-implicit-function-declaration";
+
+        var makeFilePattern = "freeimage/Makefile.*";
+        context.ReplaceRegexInFiles(makeFilePattern, @"SHAREDLIB.+\=.+", "SHAREDLIB = lib$(TARGET).so");
+
+        var buildWorkingDir = "freeimage/";
+        var env = new Dictionary<string, string>
+        {
+            { "CFLAGS", cflags },
+            { "CXXFLAGS", "-std=c++98" }
+        };
+        context.StartProcess("make", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "-f Makefile.gnu", EnvironmentVariables = env });
+
+        context.CopyFile(@"freeimage/Dist/libfreeimage.so", $"{context.ArtifactsDir}/libfreeimage.so");
     }
 }
