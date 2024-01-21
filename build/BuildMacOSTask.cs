1d9e7b45abeedb56f7d826161e03d466c2cff62c
Thomas Altenburger <thomas@flying-oak.com>
2024-01-21 19:36:25 +0100

    Try build universal binary on macOS

--- build/BuildMacOSTask.cs
+++ build/BuildMacOSTask.cs
@@ -11,19 +11,24 @@ public sealed class BuildMacOSTask : FrostingTask<BuildContext>
 
     public override void Run(BuildContext context)
     {
-        var cflags = "-Wno-implicit-function-declaration";
-        switch (RuntimeInformation.ProcessArchitecture)
-        {
-            case Architecture.Arm:
-            case Architecture.Arm64:
-                cflags += " -O3 -fPIC -fexceptions -fvisibility=hidden -DPNG_ARM_NEON_OPT=0";
-                break;
-        }
+        // disable neon instructions in case we're building for arm64
+        var cflags = "-Wno-implicit-function-declaration -O3 -fPIC -fexceptions -fvisibility=hidden -DPNG_ARM_NEON_OPT=0";
 
         var makeFilePattern = "freeimage/Makefile.*";
         context.ReplaceRegexInFiles(makeFilePattern, @"SHAREDLIB.+\=.+", "SHAREDLIB = lib$(TARGET).dylib");
         context.ReplaceTextInFiles(makeFilePattern, "cp *.so Dist/", "cp *.dylib Dist/");
         context.ReplaceTextInFiles(makeFilePattern, "$(CC) -s -shared -Wl,-soname,$(VERLIBNAME) $(LDFLAGS) -o $@ $(MODULES) $(LIBRARIES)", "$(CXX) -dynamiclib -install_name $(LIBNAME) -current_version $(VER_MAJOR).$(VER_MINOR) -compatibility_version $(VER_MAJOR) $(LDFLAGS) -o $@ $(MODULES)");
+
+        if (context.IsUniversalBinary)
+        {
+            // generate x86_64 and arm64 at once
+            context.ReplaceTextInFiles(makeFilePattern, "COMPILERFLAGS_X86_64 = -arch x86_64", "COMPILERFLAGS_X86_64 = -arch x86_64 -arch arm64");
+            context.ReplaceTextInFiles(makeFilePattern, "$(LIBTOOL) -arch_only x86_64 -o $@ $(MODULES_X86_64)", "$(LIBTOOL) -o $@ $(MODULES_X86_64)");
+            context.ReplaceTextInFiles(makeFilePattern, "$(CPP_X86_64) -arch x86_64 -dynamiclib $(LIBRARIES_X86_64) -o $@ $(MODULES_X86_64)", "$(CPP_X86_64) -arch x86_64 -arm arm64 -dynamiclib $(LIBRARIES_X86_64) -o $@ $(MODULES_X86_64)");
+            // remove lipo
+            context.ReplaceTextInFiles(makeFilePattern, "$(SHAREDLIB): $(SHAREDLIB)-i386 $(SHAREDLIB)-x86_64", "");
+            context.ReplaceTextInFiles(makeFilePattern, "$(LIPO) -create $(SHAREDLIB)-i386 $(SHAREDLIB)-x86_64 -output $(SHAREDLIB)", "");
+        }
 
         var buildWorkingDir = "freeimage/";
         var env = new Dictionary<string, string>
