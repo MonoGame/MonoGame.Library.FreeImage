b6023b4a99b9c9955dce2ea7138da406d3ddff1b
Thomas Altenburger <thomas@flying-oak.com>
2024-01-22 19:58:40 +0100

    Properly use the correct makefile

--- build/BuildMacOSTask.cs
+++ build/BuildMacOSTask.cs
@@ -12,22 +12,49 @@ public sealed class BuildMacOSTask : FrostingTask<BuildContext>
     public override void Run(BuildContext context)
     {
         // disable neon instructions in case we're building for arm64
-        var cflags = "-Wno-implicit-function-declaration -O3 -fPIC -fexceptions -fvisibility=hidden -DPNG_ARM_NEON_OPT=0";
+        var cflags = "-Os -fexceptions -fvisibility=hidden -DNO_LCMS -D__ANSI__ -DDISABLE_PERF_MEASUREMENT -DPNG_ARM_NEON_OPT=0";
 
-        var makeFilePattern = "freeimage/Makefile.*";
-        context.ReplaceRegexInFiles(makeFilePattern, @"SHAREDLIB.+\=.+", "SHAREDLIB = lib$(TARGET).dylib");
-        context.ReplaceTextInFiles(makeFilePattern, "cp *.so Dist/", "cp *.dylib Dist/");
-        context.ReplaceTextInFiles(makeFilePattern, "$(CC) -s -shared -Wl,-soname,$(VERLIBNAME) $(LDFLAGS) -o $@ $(MODULES) $(LIBRARIES)", "$(CXX) -dynamiclib -install_name $(LIBNAME) -current_version $(VER_MAJOR).$(VER_MINOR) -compatibility_version $(VER_MAJOR) $(LDFLAGS) -o $@ $(MODULES)");
+        var makeFilePattern = "freeimage/Makefile.osx";
+        // rename output to libFreeImage.dylib
+        context.ReplaceTextInFiles(
+            makeFilePattern,
+            "SHAREDLIB = lib$(TARGET)-$(VER_MAJOR).$(VER_MINOR).dylib",
+            "SHAREDLIB = lib$(TARGET).dylib");
+        context.ReplaceTextInFiles(
+            makeFilePattern, 
+            "cp *.so Dist/", 
+            "cp *.dylib Dist/");
 
         if (context.IsUniversalBinary)
         {
             // generate x86_64 and arm64 at once
-            context.ReplaceTextInFiles(makeFilePattern, "COMPILERFLAGS_X86_64 = -arch x86_64", "COMPILERFLAGS_X86_64 = -arch x86_64 -arch arm64");
-            context.ReplaceTextInFiles(makeFilePattern, "$(LIBTOOL) -arch_only x86_64 -o $@ $(MODULES_X86_64)", "$(LIBTOOL) -o $@ $(MODULES_X86_64)");
-            context.ReplaceTextInFiles(makeFilePattern, "$(CPP_X86_64) -arch x86_64 -dynamiclib $(LIBRARIES_X86_64) -o $@ $(MODULES_X86_64)", "$(CPP_X86_64) -arch x86_64 -arm arm64 -dynamiclib $(LIBRARIES_X86_64) -o $@ $(MODULES_X86_64)");
-            // remove lipo
-            context.ReplaceTextInFiles(makeFilePattern, "$(SHAREDLIB): $(SHAREDLIB)-i386 $(SHAREDLIB)-x86_64", "");
-            context.ReplaceTextInFiles(makeFilePattern, "$(LIPO) -create $(SHAREDLIB)-i386 $(SHAREDLIB)-x86_64 -output $(SHAREDLIB)", "");
+            context.ReplaceTextInFiles(
+                makeFilePattern, 
+                "COMPILERFLAGS_X86_64 = -arch x86_64",
+                "COMPILERFLAGS_X86_64 = -arch x86_64 -arch arm64");
+            // build only dynamic library
+            context.ReplaceTextInFiles(
+                makeFilePattern,
+                "FreeImage: $(STATICLIB)",
+                "FreeImage: $(SHAREDLIB)");
+            // remove arch specification to leave it to compiler flags
+            context.ReplaceTextInFiles(
+                makeFilePattern, 
+                "$(LIBTOOL) -arch_only x86_64 -o $@ $(MODULES_X86_64)",
+                "$(LIBTOOL) -o $@ $(MODULES_X86_64)");
+            context.ReplaceTextInFiles(
+                makeFilePattern,
+                "$(CPP_X86_64) -arch x86_64 -dynamiclib $(LIBRARIES_X86_64) -o $@ $(MODULES_X86_64)",
+                "$(CPP_X86_64) -dynamiclib $(LIBRARIES_X86_64) -o $@ $(MODULES_X86_64)");
+            // remove Intel 32bit
+            context.ReplaceTextInFiles(
+                makeFilePattern,
+                "$(SHAREDLIB): $(SHAREDLIB)-i386 $(SHAREDLIB)-x86_64",
+                "$(SHAREDLIB): $(SHAREDLIB)-x86_64");
+            context.ReplaceTextInFiles(
+                makeFilePattern,
+                "$(LIPO) -create $(SHAREDLIB)-i386 $(SHAREDLIB)-x86_64 -output $(SHAREDLIB)",
+                "$(LIPO) -create $(SHAREDLIB)-x86_64 -output $(SHAREDLIB)");
         }
 
         var buildWorkingDir = "freeimage/";
@@ -35,7 +62,7 @@ public sealed class BuildMacOSTask : FrostingTask<BuildContext>
         {
             { "CFLAGS", cflags }
         };
-        context.StartProcess("make", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "-f Makefile.gnu", EnvironmentVariables = env });
-        context.CopyFile(@"freeimage/Dist/libfreeimage.dylib", $"{context.ArtifactsDir}/libfreeimage.dylib");
+        context.StartProcess("make", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "-f Makefile.osx", EnvironmentVariables = env });
+        context.CopyFile(@"freeimage/Dist/libFreeImage.dylib", $"{context.ArtifactsDir}/libfreeimage.dylib");
     }
 }
