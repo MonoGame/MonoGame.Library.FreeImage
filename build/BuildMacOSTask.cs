48a525363377ef759cc5ff8265843169c4a57a17
Thomas Altenburger <thomas@flying-oak.com>
2024-01-22 20:30:57 +0100

    Patching the source to work with xcode compiler

--- build/BuildMacOSTask.cs
+++ build/BuildMacOSTask.cs
@@ -11,10 +11,35 @@ public sealed class BuildMacOSTask : FrostingTask<BuildContext>
 
     public override void Run(BuildContext context)
     {
-        // disable neon instructions in case we're building for arm64
-        var cflags = "-Os -fexceptions -fvisibility=hidden -DNO_LCMS -D__ANSI__ -DDISABLE_PERF_MEASUREMENT -DPNG_ARM_NEON_OPT=0";
+        // we need to patch the source to be compatible with the xcode 64bit compiler
+
+        // ZLib
+        context.ReplaceTextInFiles(
+            "freeimage/Source/ZLib/gzlib.c",
+            "/* Local functions */",
+            "#ifdef __APPLE__\r\n    #define _LARGEFILE64_SOURCE     /* See feature_test_macros(7) */\r\n    #include <sys/types.h>\r\n    #include <unistd.h>\r\n#endif");
+        context.ReplaceTextInFiles(
+            "freeimage/Source/ZLib/gzguts.h",
+            "#ifdef _LARGEFILE64_SOURCE",
+            "#ifdef __APPLE__\r\n    #include <unistd.h>\r\n#endif\r\n\r\n#ifdef _LARGEFILE64_SOURCE");
+        // LibJXR
+        context.ReplaceTextInFiles(
+            "freeimage/Source/LibJXR/image/decode/segdec.c",
+            "#include \"strcodec.h\"",
+            "#ifdef __APPLE__\r\n    #include <libkern/OSByteOrder.h>\r\n    #define _byteswap_ulong(x) _OSSwapInt32\r\n#endif\r\n\r\n#include \"strcodec.h\"");
+        context.ReplaceTextInFiles(
+            "freeimage/Source/LibJXR/jxrgluelib/JXRGlueJxr.c",
+            "#include <limits.h>",
+            "#ifdef __APPLE__\r\n    #include <wchar.h>\r\n#endif\r\n\r\n#include <limits.h>");
+
+        // we need to modify the makefile to produce a dynamic library
 
         var makeFilePattern = "freeimage/Makefile.osx";
+        // disable neon instructions in case we're building for arm64
+        context.ReplaceTextInFiles(
+            makeFilePattern,
+            "COMPILERFLAGS = -Os -fexceptions -fvisibility=hidden -DNO_LCMS -D__ANSI__",
+            "COMPILERFLAGS = -Os -fexceptions -fvisibility=hidden -DNO_LCMS -D__ANSI__ -DDISABLE_PERF_MEASUREMENT -DPNG_ARM_NEON_OPT=0");
         // rename output to libFreeImage.dylib
         context.ReplaceTextInFiles(
             makeFilePattern,
@@ -24,19 +49,20 @@ public sealed class BuildMacOSTask : FrostingTask<BuildContext>
             makeFilePattern, 
             "cp *.so Dist/", 
             "cp *.dylib Dist/");
+        // build only dynamic library
+        context.ReplaceTextInFiles(
+            makeFilePattern,
+            "FreeImage: $(STATICLIB)",
+            "FreeImage: $(SHAREDLIB)");
 
+        // generate x86_64 and arm64 at once
         if (context.IsUniversalBinary)
         {
-            // generate x86_64 and arm64 at once
             context.ReplaceTextInFiles(
                 makeFilePattern, 
                 "COMPILERFLAGS_X86_64 = -arch x86_64",
                 "COMPILERFLAGS_X86_64 = -arch x86_64 -arch arm64");
-            // build only dynamic library
-            context.ReplaceTextInFiles(
-                makeFilePattern,
-                "FreeImage: $(STATICLIB)",
-                "FreeImage: $(SHAREDLIB)");
+            
             // remove arch specification to leave it to compiler flags
             context.ReplaceTextInFiles(
                 makeFilePattern, 
@@ -58,11 +84,7 @@ public sealed class BuildMacOSTask : FrostingTask<BuildContext>
         }
 
         var buildWorkingDir = "freeimage/";
-        var env = new Dictionary<string, string>
-        {
-            { "CFLAGS", cflags }
-        };
-        context.StartProcess("make", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "-f Makefile.osx", EnvironmentVariables = env });
+        context.StartProcess("make", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "-f Makefile.osx"});
         context.CopyFile(@"freeimage/Dist/libFreeImage.dylib", $"{context.ArtifactsDir}/libfreeimage.dylib");
     }
 }
